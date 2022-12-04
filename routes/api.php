1bb0e1aff6c1bf0f592631da8a1e781ed99aa165
saeed <saeedhosseny1377@gmail.com>
2022-12-04 23:35:12 +0330

    update

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register' , [\App\Http\Controllers\Controller::class , 'register']);
+Route::post('login' , [\App\Http\Controllers\Controller::class , 'login']);
