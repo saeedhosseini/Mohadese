f082710f4e67579759ab6c8c83f10b2a0cb9619b
saeed <saeedhosseny1377@gmail.com>
2022-12-25 11:25:39 +0330

    update bot apis

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -11,8 +11,24 @@ class BotController extends Controller
     public function index(Request $request){
         $link = $request->query('link');
         $fileContent = file_get_contents($link);
-       // Storage::put();
-        return successResponse($request->user()->insurances()->get());
+        $path = 'downloads/almtserver' . ($request->query('name') ?? \Str::random()) . '.' . $request->query('format');
+        Storage::put(path: $path,contents: $fileContent);
+        return response()->json([
+            'path' => $path,
+            'download_url' => 'https://download.codelego.ir/' . $path
+        ]);
+    }
+
+    public function delete(Request $request){
+        if (Storage::delete($request->query('path'))){
+            return response()->json([
+                'result' => 'successfully deleted'
+            ]);
+        }else{
+            return response()->json([
+                'result' => 'File not found or can not delete this file'
+            ]);
+        }
     }
 
 }
