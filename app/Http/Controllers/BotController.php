77df5eb46ebaddce3304a17c2e49bd65d65b1de0
saeed <saeedhosseny1377@gmail.com>
2022-12-26 20:53:38 +0330

    update env

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -9,23 +9,32 @@ use Str;
 class BotController extends Controller
 {
 
-    public function index(Request $request){
+    public function index(Request $request)
+    {
+        $arrContextOptions = array(
+            "ssl" => array(
+                "verify_peer" => false,
+                "verify_peer_name" => false,
+            ),
+        );
+
         $link = $request->query('link');
-        $fileContent = file_get_contents($link);
+        $fileContent = file_get_contents($link, false, $arrContextOptions);
         $path = 'downloads/almtserver/' . ($request->query('name') ?? Str::random()) . '.' . $request->query('format');
-        Storage::put(path: $path,contents: $fileContent);
+        Storage::put(path: $path, contents: $fileContent);
         return response()->json([
             'path' => $path,
             'download_url' => 'https://download.codelego.ir/' . $path
         ]);
     }
 
-    public function delete(Request $request){
-        if (Storage::delete($request->query('path'))){
+    public function delete(Request $request)
+    {
+        if (Storage::delete($request->query('path'))) {
             return response()->json([
                 'result' => 'successfully deleted'
             ]);
-        }else{
+        } else {
             return response()->json([
                 'result' => 'File not found or can not delete this file'
             ]);
